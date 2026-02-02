8c589a7cc75f05055d210de6d868020c6293dfb0
Pavithra-Jayapal <pavithrajayapal28@gmail.com>
2026-02-02 20:45:14 +0530

    Time: 1900 ms (5%), Space: 55.9 MB (14.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,10 +1,17 @@
-//Sorting
+//Array
 class Solution {
     public int majorityElement(int[] nums) {
-        int n=nums.length;
-        Arrays.sort(nums);
-      return nums[n/2];
-       
+       int n=nums.length;
+       for(int i=0;i<n;i++){
+        int cnt=0;
+        for(int j=0;j<n;j++){
+            if(nums[i]==nums[j]){
+                cnt++;
+            }
+        }
+        if(cnt>n/2) return nums[i];
+       }
+       return -1;
     }
     
 }
