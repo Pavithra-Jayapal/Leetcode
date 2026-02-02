ee761594095122b42e42fba3d8ab89b4133ff3bb
Pavithra-Jayapal <pavithrajayapal28@gmail.com>
2026-02-02 20:51:31 +0530

    Time: 1 ms (99.82%), Space: 55.7 MB (57.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,30 +1,14 @@
-//Divide and conquer
+//counting
 class Solution {
-    public int majorityElement(int[] nums) {
-        return findMajority(nums, 0, nums.length - 1);
-    }
-    private int findMajority(int[] nums, int left, int right) {
-        if (left == right) {
-        return nums[left];
-        }
-
-        int mid = left + (right - left) / 2;
-        int leftMajor = findMajority(nums, left, mid);
-        int rightMajor = findMajority(nums, mid + 1, right);
-        if (leftMajor == rightMajor) {
-            return leftMajor;
-        }
-        int leftCount = count(nums, left, right, leftMajor);
-        int rightCount = count(nums, left, right, rightMajor);
-        return leftCount > rightCount ? leftMajor : rightMajor;
-    }
-    private int count(int[] nums, int left, int right, int target) {
-        int c = 0;
-        for (int i = left; i <= right; i++) {
-            if (nums[i] == target) {
-                c++;
+    public int majorityElement(int[] nums){
+        int count = 0;
+        int candidate = 0;
+        for (int num : nums) {
+            if (count == 0){
+                candidate = num;
             }
+         count += (num == candidate) ? 1 : -1;
         }
-        return c;
+        return candidate;
     }
 }
