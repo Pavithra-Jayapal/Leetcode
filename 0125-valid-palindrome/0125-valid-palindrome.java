708f0934fd93eaff18dd19c65b2f28a3f17a11a5
Pavithra-Jayapal <pavithrajayapal28@gmail.com>
2026-02-03 19:53:43 +0530

    Time: 4 ms (51.36%), Space: 45 MB (45.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-        while (left < right) {
-            // skip non-alphanumeric from left
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
+        //Normalize the string
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                sb.append(Character.toLowerCase(ch));
             }
-            // skip non-alphanumeric from right
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-            // compare characters (case-insensitive)
-            if (Character.toLowerCase(s.charAt(left)) 
-                != Character.toLowerCase(s.charAt(right))) {
-                return false;
-            }
-            left++;
-            right--;
         }
-        return true;
+        //Reverse the cleaned string
+        String original = sb.toString();
+        String rev= sb.reverse().toString();
+        //Compare
+        return original.equals(rev);
     }
 }
