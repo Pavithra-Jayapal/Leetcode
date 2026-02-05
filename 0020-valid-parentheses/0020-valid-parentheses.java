add52050ab10ba21772441049855ea8dcfc2ffc0
Pavithra-Jayapal <pavithrajayapal28@gmail.com>
2026-02-05 21:04:26 +0530

    Time: 135 ms (6.87%), Space: 46.8 MB (5.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
-       Stack<Character> stack=new Stack<>();
-       for(char c:s.toCharArray()){
-        if(c=='('){
-            stack.push(')');
+        while (true) {
+            String updated = s.replace("()", "").replace("{}", "").replace("[]", "");
+            if (updated.equals(s)) {
+                break;   // no more changes
+            }
+            s = updated;
         }
-        else if(c=='['){
-            stack.push(']');
-        }
-        else if(c=='{'){
-            stack.push('}');
-        }
-        else if(stack.isEmpty()|| stack.pop()!=c){
-            return false;
-        }
-       }
-       return stack.isEmpty();
-     
+        return s.isEmpty();
     }
 }
