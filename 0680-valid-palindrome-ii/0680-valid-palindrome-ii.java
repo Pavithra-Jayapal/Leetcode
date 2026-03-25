eed626a95ccca4131c9dcc67b304eeff931693ea
Pavithra-Jayapal <pavithrajayapal28@gmail.com>
2026-03-25 08:39:24 +0530

    Time: 4 ms (99.67%), Space: 47.8 MB (41.46%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    boolean isPal(String s,int left,int right){
+        while(left<right){
+        if(s.charAt(left)==s.charAt(right)){
+            left++;
+            right--;
+        }
+        else {
+            return false;
+        }
+      }  
+      return true;
+    }
+    public boolean validPalindrome(String s) {
+      int left=0;
+      int right=s.length()-1;
+      while(left<right){
+        if(s.charAt(left)==s.charAt(right)){
+            left++;
+            right--;
+        }
+        else {
+            return (isPal(s,left+1,right) || isPal(s,left,right-1));
+        }
+      }  
+      return true;
+    }
+}
