69cd95bd18da9aa2e0d8cecab3b57fda562b110e
Pavithra-Jayapal <pavithrajayapal28@gmail.com>
2026-06-18 13:47:36 +0530

    Time: 20 ms (57.2%), Space: 54.5 MB (39.85%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,21 +1,29 @@
+//memoization
 class Solution {
     public int numDistinct(String s, String t) {
        int n1=s.length();
        int n2=t.length();
        int dp[][]=new int[n1+1][n2+1];
-       for(int i=0;i<=n1;i++){
-        dp[i][0]=1;
+       for(int d[]:dp){
+        Arrays.fill(d,-1);
        }
-       for(int i=1;i<=n1;i++){
-        for(int j=1;j<=n2;j++){
-            if(s.charAt(i-1)==t.charAt(j-1)){
-                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-            }
-            else{
-                dp[i][j]=dp[i-1][j];
-            }
+       return ds(s,t,n1-1,n2-1,dp);
+    }
+    public int ds(String s, String t,int i,int j,int dp[][]){
+        if(j==-1){
+            return 1;//we may be get 1 full subsequence word
         }
-       } 
-       return dp[n1][n2];
+        if(i==-1){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        else if(s.charAt(i)==t.charAt(j)){
+            return dp[i][j]=ds(s,t,i-1,j-1,dp)+ds(s,t,i-1,j,dp);
+        }
+        else{
+            return dp[i][j]=ds(s,t,i-1,j,dp);
+        }
     }
 }
