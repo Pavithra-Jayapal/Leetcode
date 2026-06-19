1590e604eac1a43c517db47d3b44484a93e7963f
Pavithra-Jayapal <pavithrajayapal28@gmail.com>
2026-06-19 09:48:44 +0530

    Time: 29 ms (20.74%), Space: 47 MB (23.57%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,15 +1,28 @@
+//memoization
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
-        int dp[]=new int[amount+1];
-        for(int i=1;i<dp.length;i++){
-            dp[i]=Integer.MAX_VALUE;
-            for(int coin:coins){
-                if(i>=coin&&dp[i-coin]!=Integer.MAX_VALUE){
-                    dp[i]=Math.min(dp[i],dp[i-coin]+1);
-                }
+    public int cc(int coins[],int i,int j,int dp[][]){
+        if(i==0){
+            if(j%coins[0]==0){
+                return dp[i][j]=j/coins[0];
             }
+            return dp[i][j]=Integer.MAX_VALUE/2;
         }
-        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int not_take=0+cc(coins,i-1,j,dp);
+        int take=Integer.MAX_VALUE/2;
+        if(coins[i]<=j){
+            take=1+cc(coins,i,j-coins[i],dp);
+        }
+        return dp[i][j]=Math.min(take,not_take);
+    }
+    public int coinChange(int[] coins, int amount) {
+       int dp[][]=new int[coins.length][amount+1];
+       for(int d[]:dp){
+        Arrays.fill(d,-1);
+       }
+       int ans=cc(coins,coins.length-1,amount,dp);
+       return ans>=Integer.MAX_VALUE/2?-1:ans;
     }
 }
