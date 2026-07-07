e20953af74d7b9e921383361196aa2fe509ed98b
Pavithra-Jayapal <pavithrajayapal28@gmail.com>
2026-07-07 19:26:12 +0530

    Time: 7 ms (57.2%), Space: 47 MB (78.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> l=new ArrayList<>();
-        int[] ind=new int[numCourses];
+        int arr[]=new int[numCourses];
         for(int i=0;i<numCourses;i++){
             l.add(new ArrayList<>());
         }
-        for(int[] p:prerequisites){
+        for(int p[]:prerequisites){
             l.get(p[1]).add(p[0]);
-            ind[p[0]]++;
+            arr[p[0]]++;
         }
         Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<numCourses;i++){
-            if(ind[i]==0) q.offer(i);
+            if(arr[i]==0) q.add(i);
         }
         int cnt=0;
         while(!q.isEmpty()){
             int curr=q.poll();
             cnt++;
             for(int nei:l.get(curr)){
-                ind[nei]--;
-                if(ind[nei]==0) q.offer(nei);
+                arr[nei]--;
+               if(arr[nei]==0) q.add(nei);
             }
         }
         return cnt==numCourses;
